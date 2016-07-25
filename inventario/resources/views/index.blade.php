740aa4797880e51aab7ad947af3d48fca06da9ee
Greivin Calvo Aguilar <Greivin Calvo Aguilar>
2016-07-25 10:33:50 -0600

    inicio de proyecto

---
+++ inventario/resources/views/index.blade.php
@@ -0,0 +1,36 @@
+@extends('principal')
+
+@section('content')
+<div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="http://www.castellon.digital/wp-content/uploads/2015/07/estudios-salud-mental-realcionados-con-la-naturaleza-1.jpg"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/580/250/nature/2"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Left Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/580/250/nature/3"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Right Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/580/250/nature/4"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
+  </div>
+@stop
